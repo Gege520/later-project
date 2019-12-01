c9fd1c4c150540ae644395474e65fb662f967d84
Gege520 <40187142+Gege520@users.noreply.github.com>
2019-12-01 17:47:19 +0800

    Update GitApplication.java

--- git/src/main/java/com/baizhi/GitApplication.java
+++ git/src/main/java/com/baizhi/GitApplication.java
@@ -9,6 +9,7 @@ public class GitApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitApplication.class, args);
         System.out.println("更改idea中的SpringApplication");
+       System.out.println("更改github中的SpringApplication");
     }
 
 }
